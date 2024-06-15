bf930b30ce81df364798ac4b86f6603086f24049
lion <1738376480@qq.com>
2024-06-15 17:10:21 +0800

    rknn 部署 分割示例

---
+++ build/CMakeFiles/rknn_yolov5_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolov5_demo.dir/src/main.cpp.o"
+  "rknn_yolov5_demo"
+  "rknn_yolov5_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_yolov5_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
